134f20b7a2cb482b6671f2ba31403d3f7f30d46d
KTJ25 <kshitij.tech.iitkgp@gmail.com>
2024-08-11 22:20:38 +0530

    Delete Backend directory

--- Backend/routes/signup.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const signup_controller = require('../controllers/signup_controller');
-// router.get('/', (req, res) => { res.send("test") });
-router.post('/', signup_controller.signup);
-
-module.exports = router;
--- Backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const user_controller = require('../controllers/user_controller');
-const auth = require('../middlewares/auth');
-router.get('/login_check', user_controller.login_check);
-router.get('/profileedit', auth, user_controller.profileedit);
-// router.get('/get_alluser',user_controller.get_alluser);
-
-module.exports = router;
