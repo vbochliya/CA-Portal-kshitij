7f9ea9ee5401bd11f7eb5fd13bd313ddc4db0154
Neel Avniash Kotkar <neelkotkar@gmail.com>
2024-08-20 11:38:23 +0530

    Testimonials

--- Frontend/src/pages/Carousel/Card/Card.js
+++ Frontend/src/pages/Carousel/Card/Card.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from "react"
+import styles from "./card.module.css"
 
-function Card(props) {
-    return (
-        <div className={styles.elem}>
-            <div className={styles.box}><img src={props.img} alt="dp"></img></div>
-            <div className={styles.dv1}>
-                <div className={styles.p1}>{props.name} </div>
-                <div className={styles.p2}>{props.inst}, {props.city} </div>
-                <p className={styles.para}>{props.desc} </p>
+function Card(props){
+    return <>
+    <div className={styles.cardMain}>
+        <div className={styles.cardHead}>
+            <div className={styles.cardHeadImg}><img src="/logo192.png" alt="img" /></div>
+            <div className={styles.cardHeadDesc}>
+                <p>My name is Neel</p>
+                <p>My name is Neel</p>
             </div>
         </div>
-    )
+        <div className={styles.cardDesc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis eaque tempora distinctio consequatur, quod quaerat deserunt illo voluptates sint vel unde in corporis eum optio, quibusdam, velit expedita vitae! Dolore.
+        </div>
+    </div>
+    </>
 }
-
-export default Card
+export default Card;
